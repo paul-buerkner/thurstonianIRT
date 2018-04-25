0add0e19c5d0ad914891d35e3e90a956caacc282
Paul Buerkner <paul.buerkner@gmail.com>
2018-04-25 12:12:28 +0200

    predict.TIRTfit: better handle non-converging models

--- R/TIRTfit-methods.R
+++ R/TIRTfit-methods.R
@@ -24,34 +24,39 @@ predict.TIRTfit <- function(object, ...) {
   fit <- object$fit
   traits <- attributes(object$data)$traits
   if (inherits(fit, "stanfit")) {
-    out <- summary(fit, "eta")$summary %>%
-      as.data.frame() %>%
-      tibble::rownames_to_column(var = "par") %>%
-      rename(
-        estimate = "mean", se = "sd",
-        lower_ci = "2.5%", upper_ci = "97.5%"
-      ) %>%
-      select("par", "estimate", "se", "lower_ci", "upper_ci") %>%
-      tidyr::extract(
-        col = "par", into = c("par", "id", "trait"),
-        regex = "(eta)\\[([[:digit:]]+),([[:digit:]]+)\\]"
-      ) %>%
-      mutate(id = as.integer(id)) %>%
-      select(-.data$par) %>%
-      mutate(trait = as.character(factor(.data$trait, labels = traits))) %>%
-      arrange(.data$id) 
+    out <- as.data.frame(summary(fit, "eta")$summary)
+    if (nrow(out)) {
+      out <- out %>%
+        tibble::rownames_to_column(var = "par") %>%
+        rename(
+          estimate = "mean", se = "sd",
+          lower_ci = "2.5%", upper_ci = "97.5%"
+        ) %>%
+        select("par", "estimate", "se", "lower_ci", "upper_ci") %>%
+        tidyr::extract(
+          col = "par", into = c("par", "id", "trait"),
+          regex = "(eta)\\[([[:digit:]]+),([[:digit:]]+)\\]"
+        ) %>%
+        mutate(id = as.integer(id)) %>%
+        select(-.data$par) %>%
+        mutate(trait = as.character(factor(.data$trait, labels = traits))) %>%
+        arrange(.data$id)
+    }
   } else {
     if (inherits(fit, "mplusObjectTIRT")) {
       out <- fit$results[["savedata"]]
     } else if (inherits(fit, "lavaan")) {
       out <- lavaan::lavPredict(fit, ...)
     }
-    ntraits <- ncol(out)
-    out <- as.data.frame(out) %>%
-      tidyr::gather("trait", "estimate", everything()) %>%
-      mutate(id = rep(seq_len(n() / ntraits), ntraits)) %>%
-      arrange(.data$id) %>%
-      select("id", "trait", "estimate")
+    out <- as.data.frame(out)
+    if (nrow(out)) {
+      ntraits <- ncol(out)
+      out <- out %>%
+        tidyr::gather("trait", "estimate", everything()) %>%
+        mutate(id = rep(seq_len(n() / ntraits), ntraits)) %>%
+        arrange(.data$id) %>%
+        select("id", "trait", "estimate")
+    }
   }
   as_tibble(out)
 }
