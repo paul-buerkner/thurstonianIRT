60524219cf1078f58031c34334dc83cc46c566c5
Paul Buerkner <paul.buerkner@gmail.com>
2018-04-25 12:11:58 +0200

    use rstantools 1.5 build chain

---
+++ R/thurstonianIRT-package.R
@@ -0,0 +1,18 @@
+#' The 'thurstonianIRT' package.
+#'
+#' @description This package fits Thurstonian IRT models using Stan,
+#' lavaan, and Mplus.
+#'
+#' @docType package
+#' @name thurstonianIRT-package
+#' @aliases thurstonianIRT
+#' @useDynLib thurstonianIRT, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+#'
+NULL
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(thurstonianIRT)
-
-test_check("thurstonianIRT")
