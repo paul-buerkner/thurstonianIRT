ac41031fa055b8502f9d1330868bb99a9fe44591
Paul Buerkner <paul.buerkner@gmail.com>
2018-04-25 17:42:50 +0200

    minor cleaning

--- R/stanmodels.R
+++ R/stanmodels.R
@@ -26,8 +26,9 @@ stan_files <- dir(file.path(MODELS_HOME, "stan_files"),
 capture.output(
   stanmodels <- lapply(stan_files, function(f) {
     model_cppname <- sub("\\.stan$", "", basename(f))
-    stanfit <- rstan::stanc(f, allow_undefined = TRUE,
-                            obfuscate_model_name = FALSE)
+
+      stanfit <- rstan::stanc(f, allow_undefined = TRUE,
+                              obfuscate_model_name = FALSE)
     stanfit$model_cpp <- list(model_cppname = stanfit$model_name,
                               model_cppcode = stanfit$cppcode)
     return(do.call(methods::new, args = c(stanfit[-(1:3)], Class = "stanmodel",
