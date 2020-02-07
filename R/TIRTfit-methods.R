582a09302bb4f30c63ca8cbc7afb96f1b68471c1
Angus Hughes <angus.hughes@research.uwa.edu.au>
2020-02-07 16:35:02 +0800

    Added tidy output from lavaan

--- R/TIRTfit-methods.R
+++ R/TIRTfit-methods.R
@@ -89,3 +89,62 @@ predict.TIRTfit <- function(object, ...) {
   as_tibble(out)
 }
 
+
+# Get tidy df of model parameters from lavaam
+TIRT_parameters <- function(TIRTfit){
+
+  # Get information about the blocks
+  block_data <- TIRTfit$data %>%  # Reduce df to one row per item
+    distinct(item1, item2, .keep_all = TRUE)
+  nitems <- max(as.numeric(block_data$item2))  # Item 2 always has last item
+  nblocks <- max(as.numeric(block_data$block))
+  nitems_per_block <- nitems / nblocks
+
+  # Get TIRT parameters from lavaan
+  parameters <- lavaan::parameterEstimates(TIRTfit$fit)
+
+  # Get the signs to adjust loadings
+  # Need to stack both item and sign columns and get unique rows
+  signs <- tibble(item = with(block_data, c(item1, item2)),
+                  sign = with(block_data, c(sign1, sign2))) %>%
+    dplyr::distinct() %>%
+    dplyr::arrange(item) %>%
+    dplyr::pull(sign)
+
+  # Extract loadings and adjust by the sign
+  loadings <- parameters %>%
+    select(label, lhs, est, se) %>%
+    slice(seq(1, nitems * (nitems_per_block - 1),
+              by = nitems_per_block - 1)) %>%
+    mutate(label = as.numeric(gsub("\\D", "", label))) %>%
+    arrange(label) %>%
+    mutate(est = abs(est) * signs) %>%
+    rename(trait = lhs, item_number = label)
+
+  # Get pairwise variance parameters (psi_i^2 + psi_k^2)
+  # This is the diagonal of the error covariance matrix
+  vars <- parameters %>%
+    select(label, est, se) %>%
+    slice(grep(".P.", label)) %>%  #" "^[^P]*P[^P]*$" for uniquenesses
+    mutate(item1 = as.numeric(sub("P", "", sub("P[0-9]+$", "", label))),
+           item2 = as.numeric(sub(".*P", "", label))) %>%
+    arrange(item1, item2) %>%
+    select(item1, item2, est, se)
+
+  # Get item thresholds
+  thresholds <- parameters %>%
+    filter(rhs == "t1") %>%
+    select(lhs, est, se) %>%
+    mutate(item1 = as.numeric(sub("i", "", sub("i[0-9]+$", "", lhs))),
+           item2 = as.numeric(sub(".*i", "", lhs))) %>%
+    arrange(item1, item2) %>%
+    select(item1, item2, est, se)
+
+  return(
+    list(
+      loadings = loadings,
+      variances = vars,
+      thresholds = thresholds
+    )
+  )
+}
