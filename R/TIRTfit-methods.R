a94422146fb43461ac5c9b807d411330ac41b2cf
Paul Buerkner <paul.buerkner@gmail.com>
2021-08-31 17:26:14 +0200

    move and update added code

--- R/TIRTfit-methods.R
+++ R/TIRTfit-methods.R
@@ -89,3 +89,76 @@ predict.TIRTfit <- function(object, ...) {
   as_tibble(out)
 }
 
+#' Extract corrected goodness of fit statistics
+#'
+#' By default \pkg{lavaan} will return a value for degrees of
+#' freedom that ignores redundancies amongst the estimated model
+#' thresholds. This function corrects the degrees of freedom, and
+#' then recalculates the associated chi-square test statistic
+#' p-value and root mean square error of approximation (RMSEA).
+#'
+#' Note this function is currently only implemented for \pkg{lavaan}.
+#'
+#' @param object A \code{TIRTfit} object.
+#' @param ... currently unused.
+#'
+#' @return A vector containing the chi-square value, adjusted degrees of
+#'   freedom, p-value, and RMSEA.
+#'
+#' @examples
+#' # load the data
+#' data("triplets")
+#'
+#' # define the blocks of items
+#' blocks <-
+#'   set_block(c("i1", "i2", "i3"), traits = c("t1", "t2", "t3"),
+#'             signs = c(1, 1, 1)) +
+#'   set_block(c("i4", "i5", "i6"), traits = c("t1", "t2", "t3"),
+#'             signs = c(-1, 1, 1)) +
+#'   set_block(c("i7", "i8", "i9"), traits = c("t1", "t2", "t3"),
+#'             signs = c(1, 1, -1)) +
+#'   set_block(c("i10", "i11", "i12"), traits = c("t1", "t2", "t3"),
+#'             signs = c(1, -1, 1))
+#'
+#' # generate the data to be understood by 'thurstonianIRT'
+#' triplets_long <- make_TIRT_data(
+#'   data = triplets, blocks = blocks, direction = "larger",
+#'   format = "pairwise", family = "bernoulli", range = c(0, 1)
+#' )
+#'
+#' # fit the data using lavaan
+#' fit <- fit_TIRT_lavaan(triplets_long)
+#' gof(fit)
+#'
+#' @export
+gof.TIRTfit <- function(object, ...) {
+  if (!inherits(object$fit, "lavaan")) {
+    stop("gof.TIRTfit currently only works for lavaan fitted TIRT models.")
+  }
+
+  # Extract chi_sq, N, and unadjusted DF
+  chi_sq <- object$fit@test$scaled.shifted$stat
+  N <- length(unique(object$data$person))
+  df <- object$fit@test$scaled.shifted$df
+
+  # Get number of items per block to calculate redundancies
+  blocks <- unique(object$data$block)
+  redundancies <- rep(NA, length(blocks))
+  for (i in seq_along(blocks)) {
+    n_items <- nrow(unique(subset(object$data, block == blocks[i], select = itemC)))
+    redundancies[i] <- n_items * (n_items - 1) * (n_items - 2) / 6
+  }
+
+  # Adjust the DF, p-value, and recalculate the RMSEA
+  df <- df - sum(redundancies)
+  p_val <- 1 - pchisq(chi_sq, df)
+  RMSEA <- ifelse(df > chi_sq, 0, sqrt((chi_sq - df)/(df * (N - 1))))
+  gof <- c(Chi_Sq = chi_sq, df = df, p_val = p_val, RMSEA = RMSEA)
+  gof
+}
+
+#' @rdname gof.TIRTfit
+#' @export
+gof <- function(object, ...) {
+  UseMethod("gof")
+}
